86f45529de5445ede0d51a07422b3329622419de
Malhar Shembekar <malhar.shembekar@gmail.com>
2021-12-27 11:46:41 +0530

    Update Dominoes.py
    Finished the project.

--- Dominoes.py
+++ Dominoes.py
@@ -1,30 +1,14 @@
 import itertools
 import random
-import sys
-
+from sys import exit as end
+from random import randint
 from varname import argname
 
 full_set = itertools.combinations_with_replacement('0123456', 2)  # Creates a tuple with the pieces.
-stock = [list(ele) for ele in full_set]  # Tuple in tuple to list in list transform.
-for j in stock:
-    j[0] = int(j[0])
-    j[1] = int(j[1])
-player = []
-computer = []
-snake = []
-status = 0
-
-
-# Distributes pieces before the game starts.
-def pieces(a):
-    while len(a) < 7:
-        add = stock[random.randint(0, int(len(stock) - 1))]
-        if add not in a:
-            a.append(add)
-
-    for i in a:
-        if i in stock:
-            stock.remove(i)
+stock = [list([int(i), int(j)]) for i, j in full_set]
+random.shuffle(stock)
+player, computer, snake, status, gameplay = stock[:7], stock[7:14], [], "", 1
+stock = stock[14:]
 
 
 # Finds the first piece on the snake and assigns status.
@@ -55,96 +39,188 @@ def display():
 
         snake_printer()
     print()
-    for i in player:
-        print(f"{player.index(i) + 1}:{i}")
+    if len(player) > 0:
+        for i in player:
+            print(f"{player.index(i) + 1}:{i}")
     print()
-    if (status == 'player') and (len(player) != 0):
-        print("Status: It's your turn to make a move. Enter your command.")
-    elif (status == 'computer') and (len(computer) != 0):
-        input("Status: Computer is about to make a move. Press Enter to continue...")
+    if gameplay == 1:
+        if status == 'player':
+            print("Status: It's your turn to make a move. Enter your command.")
+        elif status == 'computer':
+            input("Status: Computer is about to make a move. Press Enter to continue...")
 
 
 # Player's turn
 def player_plays():
-    global status
-    case = 1
-    while True:
-        try:
-            piece_num = int(input())
-            piece = player[abs(piece_num) - 1]
-            snake_ends = [snake[0][0], snake[-1][-1]]
-            if piece_num not in range(-len(player) - 1, len(player) + 1):
-                raise ValueError
-            if set(piece).isdisjoint(snake_ends):
-                raise FileNotFoundError
-        except ValueError:
-            print("Invalid input. Please try again.")
-            case = 0
-        except FileNotFoundError:
-            print("Illegal move. Please try again.")
-            case = 0
-        else:
-            if piece_num < 0:
-                snake.insert(0, piece)
-            elif piece_num > 0:
-                snake.append(piece)
-            elif piece_num == 0:
-                if len(stock) != 0:
-                    player.append(stock[random.randint(0, len(stock) - 1)])
-                    stock.remove(player[-1])
-            if piece_num != 0:
-                if case == 1:
-                    player.remove(piece)
-            status = 'computer'
-            break
+    def is_in(a, b):
+        return not set(a).isdisjoint(b)
+    global status, gameplay, snake
+    snake_ends = [snake[0][0], snake[-1][-1]]
+    while status == "player" and gameplay == 1:  # to exit the loop when status changes.
+        if gameplay == 1:
+            if len(player) > 0:
+                try:
+                    piece_num = int(input())
+                except ValueError:
+                    print("Invalid Input. Please try again.")
+                    continue
+                piece_check = (abs(piece_num) in range(1, len(player) + 1)) or (piece_num == 0)
+                if piece_check:  # checks for valid input
+                    piece = 0 if piece_num == 0 else player[abs(piece_num) - 1]
+                    if piece != 0:
+                        snake_check = is_in(piece, snake_ends)
+                        if snake_check:  # checks for legal move
+                            if piece_num > 0:  # adding piece to the right
+                                if is_in([piece[0]], [snake_ends[-1]]):
+                                    snake.append(piece)
+                                    player.remove(piece)
+                                elif is_in([piece[-1]], [snake_ends[-1]]):
+                                    snake.append(piece[::-1])
+                                    player.remove(piece)
+                                else:
+                                    print("Illegal move. Please try again.")
+                                    continue
+                            elif piece_num < 0:  # adding piece to the left
+                                if is_in([piece[0]], [snake_ends[0]]):
+                                    snake.insert(0, piece[::-1])
+                                    player.remove(piece)
+                                elif is_in([piece[-1]], [snake_ends[0]]):
+                                    snake.insert(0, piece)
+                                    player.remove(piece)
+                                else:
+                                    print("Illegal move. Please try again.")
+                                    continue
+                            status = 'computer'
+                        else:
+                            print("Illegal move. Please try again.")
+                            continue
+                    elif piece_num == 0:  # drawing from the stock
+                        if len(stock) > 0:
+                            add_piece = stock[-1]
+                            player.append(add_piece)
+                            stock.remove(add_piece)
+                            status = 'computer'
+                        elif len(stock) == 0:
+                            gameplay = 0
+                else:
+                    print("Invalid input. Please try again.")
+                    continue
+            if len(player) == 0:
+                gameplay = 0
 
 
 # Computer's turn
 def computer_plays():
-    global status
-    i = random.randint(0, len(computer) - 1)
-    choice = random.randint(0, 1)
-    if choice == 1:
-        snake.append(computer[i])
-    elif choice == 0:
-        snake.insert(0, computer[i])
-    computer.remove(computer[i])
-    status = 'player'
+    global status, snake, gameplay
+    valid = {}  # empty dict
+    snake_ends = [snake[0][0], snake[-1][-1]]
+    case = 0
+    for i in computer:
+        if set(i).intersection(snake_ends):
+            case += 1
+            valid[case] = i
+    if gameplay == 1:
+        if len(computer) == 0:
+            gameplay = 0
+        elif len(computer) > 0:
+            if len(valid) == 0:
+                if len(stock) > 0:
+                    computer.append(stock[-1])
+                    stock.remove(stock[-1])
+                    status = 'player'
+                if len(stock) == 0:
+                    gameplay = 0
+            elif len(valid) > 0:
+                choice = valid[randint(1, len(valid))]
+                place = randint(0, 1)
+                if len(set(choice)) == 1:  # choice == [a, a]
+                    if len(set(snake_ends)) == 1:  # ends == [a, a]
+                        if place == 1:
+                            snake.append(choice)
+                        elif place == 0:
+                            snake.insert(0, choice)
+                    elif len(set(snake_ends)) == 2:  # ends == [a, b] or [b, a]
+                        if choice[0] == snake_ends[0]:
+                            snake.insert(0, choice)
+                        elif choice[0] == snake_ends[1]:
+                            snake.append(choice)
+                elif len(set(choice)) == 2:  # choice == [a, b]
+                    if len(set(snake_ends)) == 1:  # ends == [a, a]
+                        if choice[0] == snake_ends[0]:  # [a, b] and [a, a]
+                            if place == 0:
+                                snake.insert(0, choice[::-1])
+                            elif place == 1:
+                                snake.append(choice)
+                        elif choice[1] == snake_ends[0]:  # [b, a] and [a, a]
+                            if place == 0:
+                                snake.insert(0, choice)
+                            elif place == 1:
+                                snake.append(choice[::-1])
+                    elif len(set(snake_ends)) == 2:  # ends == [a, c] or [c, a] or effectively [a, b]
+                        common = set(choice).intersection(snake_ends)
+                        if len(common) == 2:
+                            if set(choice) == set(snake_ends):  # ends == effectively [a, b]
+                                if choice[0] == snake_ends[0]:  # [a, b] and [a, b]
+                                    if place == 0:
+                                        snake.insert(0, choice[::-1])
+                                    elif place == 1:
+                                        snake.append(choice[::-1])
+                                elif choice[0] == snake_ends[1]:  # [a, b] and [b, a]
+                                    if place == 0:
+                                        snake.insert(0, choice)
+                                    elif place == 1:
+                                        snake.append(choice)
+                        elif len(common) == 1:  # ends == [a, c] or [c, a]
+                            get = choice.index(*common)
+                            if choice[get] == snake_ends[0]:  # choice and [a, c]
+                                if get == 0:  # choice == [a, b]
+                                    snake.insert(0, choice[::-1])
+                                elif get == 1:  # choice == [b, a]
+                                    snake.insert(0, choice)
+                            if choice[get] == snake_ends[1]:  # choice and [c, a]
+                                if get == 0:  # choice == [a, b]
+                                    snake.append(choice)
+                                elif get == 1:  # choice == [b, a]
+                                    snake.append(choice[::-1])
+                computer.remove(choice)
+            status = 'player'
 
 
+def comp_ai(valid, computer, snake):
+    itertools.chain.from_iterable(snake + computer)
+
+# If the snake has fulfilled draw conditions
 def snake_blocked():
-    global snake
+    global snake, gameplay
     elements = list(itertools.chain.from_iterable(snake))
     if elements[0] == elements[-1]:
         if elements.count(elements[0]) == 8:
-            return 0
+            gameplay = 0
+            return True
         else:
-            return 1
+            return False
     else:
-        return 1
+        return False
 
 
-pieces(computer)
-pieces(player)
 snake.append(comparator(computer, player))
 display()
 
-while True:
-    if snake_blocked() == 1:
-        if (len(player) != 0) and (len(computer) != 0):
-            if status == 'player':
-                player_plays()
-                display()
-            elif status == 'computer':
-                computer_plays()
-                display()
-        elif len(player) == 0:
-            print("Status: The game is over. You won!")
-            sys.exit()
-        elif len(computer) == 0:
-            print("Status: The game is over. The computer won!")
-            sys.exit()
-    else:
-        print("Status: The game is over. It's a draw!")
-        sys.exit()
+while gameplay == 1:
+    if status == 'player':
+        player_plays()
+        display()
+    elif status == 'computer':
+        computer_plays()
+        display()
 
+if gameplay == 0:
+    if len(computer) == 0:
+        print("the game is over. the computer won")
+        end()
+    if len(player) == 0:
+        print("the game is over. you won")
+        end()
+    if snake_blocked() or len(stock) == 0:
+        print("the game is over. it's a draw")
+        end()
